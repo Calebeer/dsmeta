832538b8c45535485b071391813461b1a977c2d6
Teste <Teste@MacBook-Air-de-Usuario.local>
2022-11-15 11:55:06 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,21 @@
+import Notificationbutton from "./componentes/Notificationbutton/dex"
 
 
 function App() {
   return(
+    <>
     <h1>Olá mundo!</h1>
+    <Notificationbutton/>
+    <Notificationbutton/>
+    <Notificationbutton/>
+    <Notificationbutton/>
+    <Notificationbutton/>
+    <Notificationbutton/>
+    <Notificationbutton/>
+    <Notificationbutton/>
     
+    
+    </>
   )
 }
 
